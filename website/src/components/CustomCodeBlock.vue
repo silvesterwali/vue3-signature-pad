2ad43aec018ba86f64c755306cd1a349e3a66c09
selemondev <selemondev19@gmail.com>
2025-10-07 06:25:38 -0600

    chore(website): remove deprecated pkg

--- website/src/components/CustomCodeBlock.vue
+++ website/src/components/CustomCodeBlock.vue
@@ -1,26 +1,29 @@
-<script setup lang='ts'>
-import type { BundledLanguage } from 'shiki'
-import { transformerCopyButton } from '@selemondev/shiki-transformer-copy-button'
-import CodeBlock from 'shiki-code-block-vue'
+<script setup lang="ts">
+import type { BundledLanguage } from "shiki";
+import { transformerCopyButton } from "@selemondev/shiki-transformer-copy-button";
+import CodeBlock from "shiki-block-vue";
 
 const props = defineProps<{
-  code: string
-  lang: BundledLanguage
-}>()
+    code: string;
+    lang: BundledLanguage;
+}>();
 </script>
 
 <template>
-  <CodeBlock
-    :code="props.code" :lang="props.lang" :theme="{
-      light: 'vitesse-light',
-      dark: 'vitesse-dark',
-    }" :transformers="[
-      transformerCopyButton({
-        duration: 3000,
-        display: 'ready',
-        successIcon: `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='rgba(128,128,128,1)' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' viewBox='0 0 24 24'%3E%3Crect width='8' height='4' x='8' y='2' rx='1' ry='1'/%3E%3Cpath d='M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2'/%3E%3Cpath d='m9 14 2 2 4-4'/%3E%3C/svg%3E`,
-        copyIcon: `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='rgba(128,128,128,1)' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' viewBox='0 0 24 24'%3E%3Crect width='8' height='4' x='8' y='2' rx='1' ry='1'/%3E%3Cpath d='M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2'/%3E%3C/svg%3E`,
-      }),
-    ]"
-  />
+    <CodeBlock
+        :code="props.code"
+        :lang="props.lang"
+        :theme="{
+            light: 'vitesse-light',
+            dark: 'vitesse-dark',
+        }"
+        :transformers="[
+            transformerCopyButton({
+                duration: 3000,
+                display: 'ready',
+                successIcon: `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='rgba(128,128,128,1)' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' viewBox='0 0 24 24'%3E%3Crect width='8' height='4' x='8' y='2' rx='1' ry='1'/%3E%3Cpath d='M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2'/%3E%3Cpath d='m9 14 2 2 4-4'/%3E%3C/svg%3E`,
+                copyIcon: `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='rgba(128,128,128,1)' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' viewBox='0 0 24 24'%3E%3Crect width='8' height='4' x='8' y='2' rx='1' ry='1'/%3E%3Cpath d='M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2'/%3E%3C/svg%3E`,
+            }),
+        ]"
+    />
 </template>
